048de5eeba5da8d118928fdf86ee0aa3d2229625
zwzw1219 <zwzw1219@192.168.1.111>
2017-10-14 14:09:26 +0800

    1

---
+++ src/main/java/com/boot/security/server/dao/UserDao.java
@@ -0,0 +1,43 @@
+package com.boot.security.server.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import com.boot.security.server.model.User;
+
+@Mapper
+public interface UserDao {
+
+	@Options(useGeneratedKeys = true, keyProperty = "id")
+	@Insert("insert into sys_user(username, password, salt, nickname, headImgUrl, phone, telephone, email, birthday, sex, status, intro, createTime, updateTime) values(#{username}, #{password}, #{salt}, #{nickname}, #{headImgUrl}, #{phone}, #{telephone}, #{email}, #{birthday}, #{sex}, #{status}, #{intro}, now(), now())")
+	int save(User user);
+
+	@Select("select * from sys_user t where t.id = #{id}")
+	User getById(Long id);
+
+	@Select("select * from sys_user t where t.username = #{username}")
+	User getUser(String username);
+
+	@Update("update sys_user t set t.password = #{password} where t.id = #{id}")
+	int changePassword(@Param("id") Long id, @Param("password") String password);
+
+	Integer count(@Param("params") Map<String, Object> params);
+
+	List<User> list(@Param("params") Map<String, Object> params, @Param("offset") Integer offset,
+			@Param("limit") Integer limit);
+
+	@Delete("delete from sys_role_user where userId = #{userId}")
+	int deleteUserRole(Long userId);
+
+	int saveUserRoles(@Param("userId") Long userId, @Param("roleIds") List<Long> roleIds);
+
+	int update(User user);
+}
