d5ed25d93c34889e02db9caaa3397ac6b36f81f0
wei.zhang2 <wei.zhang2@baidao.com>
2017-10-16 09:11:35 +0800

    1

--- src/main/java/com/boot/security/server/SecurityApplication.java
+++ src/main/java/com/boot/security/server/SecurityApplication.java
@@ -3,6 +3,12 @@ package com.boot.security.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 启动类
+ * 
+ * @author 小威老师
+ *
+ */
 @SpringBootApplication
 public class SecurityApplication {
 
