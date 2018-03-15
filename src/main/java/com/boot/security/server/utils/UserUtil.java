15b86c907d927e6bce0e4479f8bf895f6b3e4b1b
wei.zhang2 <wei.zhang2@baidao.com>
2018-03-15 11:07:22 +0800

    匿名用户判断

--- src/main/java/com/boot/security/server/utils/UserUtil.java
+++ src/main/java/com/boot/security/server/utils/UserUtil.java
@@ -1,5 +1,7 @@
 package com.boot.security.server.utils;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -10,7 +12,13 @@ public class UserUtil {
 	public static LoginUser getLoginUser() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (authentication != null) {
-			return (LoginUser) authentication.getPrincipal();
+			if (authentication instanceof AnonymousAuthenticationToken) {
+				return null;
+			}
+
+			if (authentication instanceof UsernamePasswordAuthenticationToken) {
+				return (LoginUser) authentication.getPrincipal();
+			}
 		}
 
 		return null;
