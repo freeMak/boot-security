0a2edfae4e08702ed406b6c549f38f4269086bc5
wei.zhang2 <wei.zhang2@baidao.com>
2018-05-16 10:59:46 +0800

    1

--- src/main/java/com/boot/security/server/service/impl/TokenServiceImpl.java
+++ src/main/java/com/boot/security/server/service/impl/TokenServiceImpl.java
@@ -6,7 +6,6 @@ import java.util.concurrent.TimeUnit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.stereotype.Service;
 
 import com.boot.security.server.dto.LoginUser;
 import com.boot.security.server.dto.Token;
@@ -21,7 +20,7 @@ import com.boot.security.server.service.TokenService;
  *
  */
 @Deprecated
-@Service
+//@Service
 public class TokenServiceImpl implements TokenService {
 
 	/**
